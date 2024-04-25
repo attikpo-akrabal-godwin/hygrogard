06673e99ba92957bee9ee23a5e986bc704ab7c82
Lonie <lonierachid@gmail.com>
2024-04-25 23:56:37 +0000

    ..;

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, ScrollView, RefreshControl} from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { socket } from './src/socket.js';
 import Navbar from './components/NavBar.js'
 import ProgressCircle from './components/ProgressCircle.js'
@@ -7,7 +7,6 @@ import {
   LineChart,
 } from "react-native-chart-kit";
 import { useEffect, useState } from 'react';
-import ComposantCouleur from './components/ComposantCouleur.js'
 
 export default function App() {
   const data = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
@@ -15,19 +14,9 @@ export default function App() {
   const [i, setI] = useState(0);
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [fooEvents, setFooEvents] = useState([]);
-  const [lanpe,setLampe] = useState(true)
-  const [lines,setLines] = useState([0,0,0,0,0,0,0,0,0,0,0])
-  const [refreshing, setRefreshing] = useState(false);
+  const [lanpe, setLampe] = useState(true)
+  const [lines, setLines] = useState([])
   const maxvalue = 1024;
-
-  const onRefresh = () => {
-    setRefreshing(true);
-    connection()
-    setTimeout(() => {
-      setRefreshing(false);
-    }, 100); // Temps fictif pour simuler une opération de rafraîchissement
-  };
-
   useEffect(() => {
     function onConnect() {
       setIsConnected(true);
@@ -52,12 +41,12 @@ export default function App() {
     };
   }, []);
 
-  useEffect(()=>{
-    connection()
-    socket.emit('auth',{name:'mobile-app'})
+  useEffect(() => {
+    socket.connect()
+    socket.emit('auth', { name: 'mobile-app' })
     socket.on('sensor', (data) => {
       setProgress(data)
-      
+
     });
     socket.on('capteur', (data) => {
       console.log(data)
@@ -65,66 +54,57 @@ export default function App() {
       setProgress(data)
     });
 
-    
-  },[])
-  function connection() {
-    socket.connect();
-  }
+
+  }, [])
   function handleClick() {
     socket.emit('on/off', !lanpe);
     setLampe(!lanpe)
     console.log(!lanpe)
   }
   return (
-  <ScrollView
-    refreshControl={
-      <RefreshControl
-        refreshing={refreshing}
-        onRefresh={onRefresh}
-      />
-    }>
-      <View >
-        <Navbar title="Hydroguard" lampe={isConnected} />
-        <ProgressCircle progress={progress} radius={90} strokeWidth={10} />
-        <LineChart
-          data={{
-            datasets: [
-              {
-                data: lines,
-              }
-            ]
-          }}
-          width={400} // from react-native
-          height={220}
-          yAxisLabel=""
-          yAxisSuffix=""
-          yAxisInterval={1} // optional, defaults to 1
-          chartConfig={{
-            backgroundColor: "#007bff", // Couleur de fond
-            backgroundGradientFrom: "#007bff", // Dégradé de couleur de fond (début)
-            backgroundGradientTo: "#007bff", // Dégradé de couleur de fond (fin)
-            decimalPlaces: 2,
-            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`, // Couleur de la ligne
-            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`, // Couleur des étiquettes
-            style: {
-              borderRadius: 16
-            },
-            propsForDots: {
-              r: "6",
-              strokeWidth: "2",
-              stroke: "#007bff" // Couleur des points
+    <View >
+      <Navbar title="Hydroguard" />
+      <ProgressCircle progress={progress} radius={90} strokeWidth={10} />
+      <LineChart
+        data={{
+          datasets: [
+            {
+              data: lines,
             }
-          }}
-          bezier
-          style={{
-            marginVertical: 8,
+          ]
+        }}
+        width={400} // from react-native
+        height={220}
+        yAxisLabel=""
+        yAxisSuffix=""
+        yAxisInterval={1} // optional, defaults to 1
+        chartConfig={{
+          backgroundColor: "#007bff", // Couleur de fond
+          backgroundGradientFrom: "#007bff", // Dégradé de couleur de fond (début)
+          backgroundGradientTo: "#007bff", // Dégradé de couleur de fond (fin)
+          decimalPlaces: 2,
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`, // Couleur de la ligne
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`, // Couleur des étiquettes
+          style: {
             borderRadius: 16
-          }}
-        />
-        <Button onPress={handleClick} title='click' />
-        <StatusBar style="auto" />
-      </View>
-  </ScrollView>
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#007bff" // Couleur des points
+          }
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
+      <Button onPress={handleClick} title='click' />
+
+
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
