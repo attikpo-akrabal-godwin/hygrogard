d161a47d98a5077db3a5e995d81d1427462d7715
Lonie <lonierachid@gmail.com>
2024-04-26 00:42:09 +0000

    ..;

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, ScrollView, RefreshControl, SafeAreaView, View } from 'react-native';
 import { socket } from './src/socket.js';
 import Navbar from './components/NavBar.js'
 import ProgressCircle from './components/ProgressCircle.js'
 import {
+  BarChart,
   LineChart,
+  PieChart,
 } from "react-native-chart-kit";
 import { useEffect, useState } from 'react';
+import ComposantCouleur from './components/ComposantCouleur.js'
 
 export default function App() {
   const data = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
@@ -15,8 +18,18 @@ export default function App() {
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [fooEvents, setFooEvents] = useState([]);
   const [lanpe, setLampe] = useState(true)
-  const [lines, setLines] = useState([])
+  const [lines, setLines] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+  const [refreshing, setRefreshing] = useState(false);
   const maxvalue = 1024;
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    connection()
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 100); // Temps fictif pour simuler une opération de rafraîchissement
+  };
+
   useEffect(() => {
     function onConnect() {
       setIsConnected(true);
@@ -42,7 +55,7 @@ export default function App() {
   }, []);
 
   useEffect(() => {
-    socket.connect()
+    connection()
     socket.emit('auth', { name: 'mobile-app' })
     socket.on('sensor', (data) => {
       setProgress(data)
@@ -56,54 +69,82 @@ export default function App() {
 
 
   }, [])
+  function connection() {
+    socket.connect();
+  }
   function handleClick() {
     socket.emit('on/off', !lanpe);
     setLampe(!lanpe)
     console.log(!lanpe)
   }
   return (
-    <View >
-      <Navbar title="Hydroguard" />
-      <ProgressCircle progress={progress} radius={90} strokeWidth={10} />
-      <LineChart
-        data={{
-          datasets: [
-            {
-              data: lines,
+    <SafeAreaView style={{ flex: 1 }}>
+      <Navbar title="Hydroguard" lampe={isConnected} />
+      <ScrollView
+        style={{ padding: 12 }}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        }>
+
+        {/* Section 1 */}
+        <Title title="Progress Circle" />
+        <Text style={styles.descriptionText}>
+          Description of the progress circle displayed on bottom of this. Maybe not important.
+        </Text>
+        <View style={styles.progressContainer}>
+          <ProgressCircle
+            progress={progress}
+            radius={90}
+            strokeWidth={10}
+          />
+        </View>
+
+        {/* Section 2 */}
+        <Title title="Line Chart" />
+        <Text style={styles.descriptionText}>
+          Description of the Line chart displayed on bottom of this. Maybe not important.
+        </Text>
+        <LineChart
+          data={{ datasets: [{ data: lines, }] }}
+          width={400} // from react-native
+          height={220}
+          yAxisLabel=""
+          yAxisSuffix=""
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: "#007bff", // Couleur de fond
+            backgroundGradientFrom: "#007bff", // Dégradé de couleur de fond (début)
+            backgroundGradientTo: "#007bff", // Dégradé de couleur de fond (fin)
+            decimalPlaces: 2,
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`, // Couleur de la ligne
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`, // Couleur des étiquettes
+            propsForDots: {
+              r: "6", strokeWidth: "2", stroke: "#007bff"
             }
-          ]
-        }}
-        width={400} // from react-native
-        height={220}
-        yAxisLabel=""
-        yAxisSuffix=""
-        yAxisInterval={1} // optional, defaults to 1
-        chartConfig={{
-          backgroundColor: "#007bff", // Couleur de fond
-          backgroundGradientFrom: "#007bff", // Dégradé de couleur de fond (début)
-          backgroundGradientTo: "#007bff", // Dégradé de couleur de fond (fin)
-          decimalPlaces: 2,
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`, // Couleur de la ligne
-          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`, // Couleur des étiquettes
-          style: {
-            borderRadius: 16
-          },
-          propsForDots: {
-            r: "6",
-            strokeWidth: "2",
-            stroke: "#007bff" // Couleur des points
-          }
-        }}
-        bezier
-        style={{
-          marginVertical: 8,
-          borderRadius: 16
-        }}
-      />
-      <Button onPress={handleClick} title='click' />
+          }}
+        />
 
+        {/* <StatusBar style="auto" /> */}
+      </ScrollView>
+      <View style={{ padding: 12 }}>
+        <Button onPress={handleClick} title='Click !' />
+      </View>
+    </SafeAreaView>
+  );
+}
 
-      <StatusBar style="auto" />
+const Title = ({ title }) => {
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <View style={{
+        height: 12, width: 12,
+        borderRadius: 12,
+        backgroundColor: "lightgreen", marginRight: 10
+      }}></View>
+      <Text style={styles.titleText}>{title}</Text>
     </View>
   );
 }
@@ -115,4 +156,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  progressContainer: {
+    borderWidth: 0.3,
+    alignItems: "flex-start",
+    backgroundColor: "whitesmoke",
+    borderColor: "grey",
+    marginBottom: 20
+  },
+  titleText: {
+    fontSize: 18,
+    color: "#24292E",
+    // marginBottom: 6
+  },
+  descriptionText: {
+    fontSize: 14,
+    color: "grey",
+    fontWeight: "400",
+    marginBottom: 10
+  }
 });
--- components/ComposantCouleur.js
+++ components/ComposantCouleur.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 const ComposantCouleur = ({ valeur }) => {
-    return (
-        <View style={{
-          width: 10,
-          height: 10,
-          borderRadius: 5,
-          backgroundColor: valeur ? 'green' : 'red'
-        }} />
-      );
+  return (
+    <View style={{
+      width: 20,
+      height: 20,
+      borderRadius: 20,
+      backgroundColor: valeur ? 'green' : "rgba(255, 0, 0, 0.6)",
+      borderWidth: 1,
+      borderColor: "red",
+      marginLeft: 12,
+      position: "absolute",
+      top: -2,
+      right: 0
+    }} />
+  );
 };
 
 export default ComposantCouleur;
